d85c69cf57952a3ec5af1f1a3867bf3d44f58338
Roy <cpicado@uamv.edu.ni>
2026-05-27 01:41:43 -0600

    COMMIT

---
+++ Scripts/GameData.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@export var regular_cards: Array[CardData] = []
+@export var special_cards: Array[CardData] = []
+var p_deck: Array[CardData] = []
+var p_special_cards: Array[CardData] = []
+var p_max_healh: int = 100
+var p_health: int = 100
+var p_run: int = 0
